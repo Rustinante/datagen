c4269698aa87783c95febbcec8a3c7bb388d3bee
Aaron <aaronzhouqian@ucla.edu>
2018-03-16 13:13:01 -0700

    update run script

--- gkm_datagen/run.sh
+++ gkm_datagen/run.sh
@@ -10,11 +10,26 @@ output_dirname=$2
 echo "narrowpeak filename: ${narrow_filename}"
 echo "output_dirname: ${output_dirname}"
 
-python3 narrowpeak_to_fa.py $1 $2
+. /u/local/Modules/default/init/modules.sh
+module load python/3.6.1
+
+
+python3 narrowpeak_to_fa.py $1 $2 || exit 1
+
 
 cd ..
-python3 -m gkm_datagen.species_letters_from_coord gkm_datagen/$2/$2.train.pos.coord gkm_datagen/$2
-python3 -m gkm_datagen.species_letters_from_coord gkm_datagen/$2/$2.train.neg.coord gkm_datagen/$2
-python3 -m gkm_datagen.species_letters_from_coord gkm_datagen/$2/$2.test.pos.coord  gkm_datagen/$2
-python3 -m gkm_datagen.species_letters_from_coord gkm_datagen/$2/$2.test.neg.coord gkm_datagen/$2
+python3 -m gkm_datagen.species_letters_from_coord gkm_datagen/$2/$2.train.pos.coord gkm_datagen/$2 || exit 1
+python3 -m gkm_datagen.species_letters_from_coord gkm_datagen/$2/$2.train.neg.coord gkm_datagen/$2 || exit 1
+python3 -m gkm_datagen.species_letters_from_coord gkm_datagen/$2/$2.test.pos.coord  gkm_datagen/$2 || exit 1
+python3 -m gkm_datagen.species_letters_from_coord gkm_datagen/$2/$2.test.neg.coord gkm_datagen/$2 || exit 1
 
+
+echo "=> transporting files"
+cd gkm_datagen
+mkdir /u/home/a/aaronzho/project-ernst/lsgkm/tests/$2
+python3 transport_files.py $2 /u/home/a/aaronzho/project-ernst/lsgkm/tests/$2 || exit 1
+
+
+echo "=> submitting jobs"
+cd /u/home/a/aaronzho/project-ernst/lsgkm/tests/
+qsub -cwd -V -N GKMJOB -pe shared 2 -l h_data=5G,h_rt=24:00:00,highp -M $HOME -m a -t 1-100:1 jobarray.sh
