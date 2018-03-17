53b23aa21a97ce025804c900c77549dfd0fe1cdd
Aaron <aaronzhouqian@ucla.edu>
2018-03-16 23:54:25 -0700

    update scripts

--- gkm_datagen/run.sh
+++ gkm_datagen/run.sh
@@ -26,7 +26,7 @@ python3 -m gkm_datagen.species_letters_from_coord gkm_datagen/$2/$2.test.neg.coo
 
 echo "=> transporting files"
 cd gkm_datagen
-mkdir /u/home/a/aaronzho/project-ernst/lsgkm/tests/$2
+mkdir /u/home/a/aaronzho/project-ernst/lsgkm/tests/${output_dirname}
 python3 transport_files.py $2 /u/home/a/aaronzho/project-ernst/lsgkm/tests/$2 || exit 1
 
 
@@ -34,6 +34,6 @@ echo "=> submitting jobs"
 cd /u/home/a/aaronzho/project-ernst/lsgkm/tests/${output_dirname}
 echo "
 #!/bin/bash
-../task.sh $SGE_TASK_ID ${output_dirname}
+../task.sh \${SGE_TASK_ID} ${output_dirname}
 " > jobarray.sh
-qsub -cwd -V -N GKMJOB -pe shared 2 -l h_data=5G,h_rt=24:00:00,highp -M $HOME -m a -t 1-100:1 jobarray.sh
+qsub -cwd -V -N ${output_dirname}_job -pe shared 2 -l h_data=5G,h_rt=24:00:00,highp -M $HOME -m a -t 1-100:1 jobarray.sh
--- gkm_datagen/task.sh
+++ gkm_datagen/task.sh
@@ -6,15 +6,14 @@ dirname=$2
 echo "=> species_index: ${species_index}"
 echo "=> dirname ${dirname}"
 
-cd ${dirname}
 
 ../../bin/gkmtrain -T25 -t0 -l10 -k6 -d3 -c1 ${dirname}.train.pos.coord.mult_species.at1.00/${species_index}_train_pos.fa ${dirname}.train.neg.coord.mult_species.at1.00/${species_index}_train_neg.fa species.${species_index} || exit 1
 
 echo "=> Finished training species ${species_index}"
 
-../../bin/gkmpredict -T25 ${dirname}.test.pos.coord.mult_species/${species_index}_test_pos.fa species_model/species.${species_index}.model.txt ${species_index}.pos_predict || exit 1
+../../bin/gkmpredict -T25 ${dirname}.test.pos.coord.mult_species.at1.00/${species_index}_test_pos.fa species_model/species.${species_index}.model.txt ${species_index}.pos_predict || exit 1
 
-../../bin/gkmpredict -T25 ${dirname}.test.neg.coord.mult_species/${species_index}_test_neg.fa species_model/species.${species_index}.model.txt ${species_index}.neg_predict || exit 1
+../../bin/gkmpredict -T25 ${dirname}.test.neg.coord.mult_species.at1.00/${species_index}_test_neg.fa species_model/species.${species_index}.model.txt ${species_index}.neg_predict || exit 1
 
 echo "=> Finished predicting species ${species_index}"
 
