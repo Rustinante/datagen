36a4f6c53ff38fc63a8efb7672b117bb07fb8fce
Aaron <aaronzhouqian@ucla.edu>
2018-03-28 20:12:42 -0700

    remove extra characters

--- gkm_datagen/start_job.sh
+++ gkm_datagen/start_job.sh
@@ -5,4 +5,4 @@ module load python/3.6.1
 
 # python3 submit_massive_jobs.py dnase.np narrowfiles/dnase_narrowpeak/ $((${SGE_TASK_ID} - 1))
 
-python3 submit_massive_jobs.py histone.np narrowfiles/histone_narrowpeak/ $((${SGE_TASK_ID} - 1))sh
+python3 submit_massive_jobs.py histone.np narrowfiles/histone_narrowpeak/ $((${SGE_TASK_ID} - 1))
