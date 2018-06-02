829d004d1a65a9094a8ebf5c917c7b62fbb5b321
Aaron <aaronzhouqian@ucla.edu>
2018-06-02 12:27:50 -0700

    fix the bug by serializing the remaining chunk of data

--- constrained_data/generate_constrained_element_data.py
+++ constrained_data/generate_constrained_element_data.py
@@ -108,6 +108,15 @@ def generate(coord_filename, output_filename):
                 print(f'{line_index}/{line_count} = {line_index/line_count:.2%} in {time.time() - stamp:.4f}s'
                       f' current serializing index: {serializing_index}',
                       end='\r')
+        
+        # Serialize the remaining data
+        if states_list:
+            for vector in states_list:
+                feature_data[serializing_index] = vector
+                serializing_index += 1
+    
+            print(f'{serializing_index}/{line_count} = {serializing_index/line_count:.2%} in {time.time() - stamp:.4f}s'
+                  f' current serializing index: {serializing_index}')
     
     print(f'\n#ones: {one_state_count}  #zeros: {zero_state_count}')
     close_file_dict(chrom_file_dict)
--- generate_chrom_state_data.py
+++ generate_chrom_state_data.py
@@ -100,7 +100,17 @@ def generate(coord_filename):
                 print(f'{line_index}/{line_count} = {line_index/line_count:.2%} in {time.time() - stamp:.4f}s'
                       f' current serializing index: {serializing_index}',
                       end='\r')
+        
+        # Serialze the remaining data
+        if states_list:
+            for vector in states_list:
+                feature_data[serializing_index] = vector
+                serializing_index += 1
     
+            print(f'{serializing_index}/{line_count} = {serializing_index/line_count:.2%} in {time.time() - stamp:.4f}s'
+                  f' current serializing index: {serializing_index}',
+                  end='\r')
+         
     print(f'\n-> Number of missing states: {num_missing_states}')
     
     close_file_dict(chrom_state_file_dict)
