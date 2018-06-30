be1681863256532226ccd78f6eeae3fe2867e9a6
Aaron <aaronzhouqian@ucla.edu>
2018-06-29 22:03:14 -0700

    generate 5 channel for 100 species

--- step3_extend_five_channel.py
+++ step3_extend_five_channel.py
@@ -7,6 +7,14 @@ import time
 from binary_search import search, get_location_from_line
 
 
+def get_line_count(filename):
+    count = 0
+    with open(filename, 'r') as file:
+        for _ in file:
+            count += 1
+    return count
+
+
 class LineCache:
     def __init__(self):
         self.capacity = 1000
@@ -87,7 +95,7 @@ def scan_through_line_for_number(alignment_file, start_line_hint, number):
     return None
 
 
-def extend_dataset(chr, purpose):
+def extend_dataset(chrom, purpose):
     checkpoint_time_str = time.strftime('%a %b %d %Y %H:%M:%S UTC%z', time.localtime(time.time()))
     print('Current time: {}'.format(checkpoint_time_str))
     
@@ -95,41 +103,51 @@ def extend_dataset(chr, purpose):
     
     array_list = []
     
-    coordinate_filename = os.path.join('data', '{}_{}'.format(chr, purpose))
-    alignment_filename = '{}_maf_sequence.csv'.format(chr)
-    hdf5_filename = '{}_{}.short.hdf5'.format(chr, purpose)
-    species_indices = [11, 54, 73, 83]
-    number_of_species = len(species_indices) + 1
+    coordinate_filename = os.path.join('data', '{}_{}'.format(chrom, purpose))
+    alignment_filename = '{}_maf_sequence.csv'.format(chrom)
+    hdf5_filename = '{}_{}.hundred.hdf5'.format(chrom, purpose)
+    num_species = 100
     
     print('=> coordinate_filename: {}'.format(coordinate_filename))
     print('=> alignment_filename: {}'.format(alignment_filename))
     print('=> target hdf5_filename: {}'.format(hdf5_filename))
     file_byte_size = os.stat(alignment_filename).st_size
     
-    with open(coordinate_filename, 'r') as file, open(alignment_filename, 'r') as alignment_file:
+    line_count = get_line_count(coordinate_filename)
+    
+    flanking_number = 400
+    seq_len = 200 + 2 * flanking_number
+    feature_dim = 5
+    
+    serializing_index = 0
+    
+    with open(coordinate_filename, 'r') as coord_file, open(alignment_filename, 'r') as alignment_file, \
+            h5py.File(hdf5_filename, 'w') as hdf5_file:
+        
+        feature_group = hdf5_file.create_group('feature')
+        feature_data = feature_group.create_dataset('data', (line_count * 2, num_species, seq_len, feature_dim), dtype='uint8')
+        
         header = alignment_file.readline().strip().split(',')
         human_index = header.index('hg19')
         
         processed_line_count = 0
         start_time = time.time()
-        flanking_number = 0
         
-        seq_len = 200 + 2 * flanking_number
-        feature_dim = 5
-        
-        for line in file:
+        for line in coord_file:
             processed_line_count += 1
             (start_coordinate, sequence) = line.strip().split(',')
             start_coordinate = int(start_coordinate)
             
-            alignment_matrix = np.zeros((seq_len, number_of_species, feature_dim), dtype='uint8')
-            
-            for letter_index, hg_letter in enumerate(sequence):
-                alignment_matrix[letter_index, 0, :] = mapping[hg_letter]
+            # 1000 x 100 x 5
+            alignment_matrix = np.zeros((seq_len, num_species, feature_dim), dtype='uint8')
             
             start_line_hint = None
-            for letter_index, coordinate in enumerate(
-                    range(start_coordinate - flanking_number, start_coordinate + 200 + flanking_number)):
+            for letter_index, (hg_letter, coordinate) in enumerate(
+                    zip(sequence, range(start_coordinate - flanking_number, start_coordinate + 200 + flanking_number))):
+                
+                # insert the human letter first
+                alignment_matrix[letter_index, 0, :] = mapping[hg_letter]
+                
                 if coordinate in cache:
                     cached_result = cache[coordinate]
                     alignment_matrix[letter_index][1:, :] = cached_result[0]
@@ -150,44 +168,48 @@ def extend_dataset(chr, purpose):
                     del tokens[human_index]
                     del tokens[0]
                     
-                    # aligned_letters is 100x4
+                    # aligned_letters is 100x5
                     aligned_letters = alignment_matrix[letter_index]
                     
-                    for row_number, species_index in enumerate(species_indices):
-                        letter = tokens[species_index]
+                    for row_number, letter in enumerate(tokens):
                         # +1 because we put hg19 in the first row
                         aligned_letters[row_number + 1, :] = mapping[letter]
                     
-                    # for species_index, letter in enumerate(tokens):
-                    # aligned_letters is 100x4
-                    # +1 because we put hg19 in the first row
-                    # aligned_letters[species_index + 1, :] = mapping[letter]
-                    
                     cache[coordinate] = (aligned_letters[1:, :], start_line_hint)
+                
+                elif hg_letter != 'N' and hg_letter != 'n':
+                    aligned_letters = alignment_matrix[letter_index]
+                    # num_species minus the human
+                    for species_index in range(num_species - 1):
+                        aligned_letters[species_index + 1, :] = mapping['X']
             
             array_list.append(alignment_matrix.transpose((1, 0, 2)))
             
-            if processed_line_count % 1000 == 0:
+            if processed_line_count % 100 == 1:
+                for matrix in array_list:
+                    feature_data[serializing_index] = matrix
+                    # For the reverse complement strand
+                    feature_data[serializing_index + line_count] = matrix[::-1]
+                    serializing_index += 1
+                
+                array_list = []
+                
                 elapsed_time = time.time() - start_time
                 time_per_line = elapsed_time / processed_line_count
-                print('Processed {} lines in {:5f}s, averaging: {:5f}s per line'
-                      .format(processed_line_count, elapsed_time, time_per_line))
-    
-    stamp = time.time()
-    print('=> Serializing...')
-    with h5py.File(hdf5_filename, 'w') as file:
-        feature_group = file.create_group('feature')
+                print(f'{processed_line_count}/{line_count} = {processed_line_count/line_count:.2%} in {elapsed_time:.4f}s'
+                      f'averaging {time_per_line}s per line'
+                      f' current serializing index: {serializing_index}',
+                      end='\r')
         
-        array_list_length = len(array_list)
-        
-        feature_data = feature_group.create_dataset('data', (array_list_length, number_of_species, seq_len, feature_dim), dtype='uint8')
-        for index, matrix in enumerate(array_list):
-            feature_data[index] = matrix
-            
-            if index % 1000 == 0:
-                print("{}/{} in {:5f}s".format(index, array_list_length, time.time() - stamp))
-    
-    print('=> Finished serializeing in {:.5f}s'.format(time.time() - stamp))
+        # Serializing the remaining data
+        if array_list:
+            for matrix in array_list:
+                feature_data[serializing_index] = matrix
+                feature_data[serializing_index + line_count] = matrix[::-1]
+                serializing_index += 1
+            print(f'{processed_line_count}/{line_count} = {processed_line_count/line_count:.2%} in {elapsed_time:.4f}s'
+                  f'averaging {time_per_line}s per line'
+                  f' current serializing index: {serializing_index}')
 
 
 if __name__ == '__main__':
