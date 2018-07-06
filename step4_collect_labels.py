ffa39cac739734bdb672f3824c04b7228266c7f5
Aaron <aaronzhouqian@ucla.edu>
2018-07-05 22:01:57 -0700

    collect labels for hundred species alignment

--- step4_collect_labels.py
+++ step4_collect_labels.py
@@ -17,8 +17,8 @@ def collect_labels():
         
         for line_index, line in enumerate(train_coord_file):
             tokens = line.split()
-            chr, start, stop = tokens[0], int(tokens[1]), int(tokens[2])
-            labels_dict[chr].append(training_labels[:, line_index])
+            chrom, start, stop = tokens[0], int(tokens[1]), int(tokens[2])
+            labels_dict[chrom].append(training_labels[:, line_index])
             
             if line_index % 10000 == 9999:
                 elapsed = time.time() - stamp
@@ -30,24 +30,18 @@ def collect_labels():
 
 
 def add_labels_to_dataset(labels_dict):
-    for chr, label_list in labels_dict.items():
+    for chrom, label_list in labels_dict.items():
         label_array = np.array(label_list)
         print('=> {} has {} labels with a corresponding array of shape {}'
-              .format(chr, len(label_list), label_array.shape))
+              .format(chrom, len(label_list), label_array.shape))
         
-        hdf5_filename = '{}_train.short.hdf5'.format(chr)
-        revcomp_filename = '{}_train.revcomp.short.hdf5'.format(chr)
+        hdf5_filename = '{}_train.hundred.hdf5'.format(chrom)
         print('=> opening {}'.format(hdf5_filename))
         
         with h5py.File(hdf5_filename, 'r+') as hdf5_file:
             group = hdf5_file.create_group('label')
-            dataset = group.create_dataset('data', data=label_array, dtype='uint8')
+            group.create_dataset('data', data=label_array, dtype='uint8')
         print('=> added the labels to {}'.format(hdf5_filename))
-        
-        with h5py.File(revcomp_filename, 'r+') as hdf5_file:
-            group = hdf5_file.create_group('label')
-            dataset = group.create_dataset('data', data=label_array, dtype='uint8')
-        print('=> added the labels to {}'.format(revcomp_filename))
         
 
 def collect_validation_labels():
@@ -64,8 +58,8 @@ def collect_validation_labels():
         
         for line_index, line in enumerate(valid_coord_file):
             tokens = line.split()
-            chr = tokens[0]
-            labels_dict[chr].append(validation_labels[line_index, :])
+            chrom = tokens[0]
+            labels_dict[chrom].append(validation_labels[line_index, :])
             
             if line_index % 1000 == 999:
                 elapsed = time.time() - stamp
@@ -77,25 +71,19 @@ def collect_validation_labels():
 
 
 def add_labels_to_validation_dataset(labels_dict):
-    for chr, label_list in labels_dict.items():
+    for chrom, label_list in labels_dict.items():
         label_array = np.array(label_list)
         print('=> {} has {} labels with a corresponding array of shape {}'
-              .format(chr, len(label_list), label_array.shape))
+              .format(chrom, len(label_list), label_array.shape))
         
-        hdf5_filename = '{}_valid.short.hdf5'.format(chr)
-        revcomp_filename = '{}_valid.revcomp.short.hdf5'.format(chr)
+        hdf5_filename = '{}_valid.hundred.hdf5'.format(chrom)
         print('=> opening {}'.format(hdf5_filename))
         
         with h5py.File(hdf5_filename, 'r+') as hdf5_file:
             group = hdf5_file.create_group('label')
-            dataset = group.create_dataset('data', data=label_array, dtype='uint8')
+            group.create_dataset('data', data=label_array, dtype='uint8')
         print('=> added the labels to {}'.format(hdf5_filename))
         
-        with h5py.File(revcomp_filename, 'r+') as hdf5_file:
-            group = hdf5_file.create_group('label')
-            dataset = group.create_dataset('data', data=label_array, dtype='uint8')
-        print('=> added the labels to {}'.format(revcomp_filename))
-
 
 def collect_test_labels():
     print('=> collecting test labels')
@@ -109,8 +97,8 @@ def collect_test_labels():
     with open('test_coord', 'r') as test_coord_file:
         for line_index, line in enumerate(test_coord_file):
             tokens = line.split()
-            chr = tokens[0]
-            labels_dict[chr].append(test_labels[line_index, :])
+            chrom = tokens[0]
+            labels_dict[chrom].append(test_labels[line_index, :])
             
             if line_index % 1000 == 999:
                 elapsed_time = time.time() - stamp
@@ -122,35 +110,29 @@ def collect_test_labels():
 
 
 def add_labels_to_test_dataset(labels_dict):
-    for chr, label_list in labels_dict.items():
+    for chrom, label_list in labels_dict.items():
         label_array = np.array(label_list)
         print('=> {} has {} labels with a corresponding array of shape {}'
-              .format(chr, len(label_list), label_array.shape))
+              .format(chrom, len(label_list), label_array.shape))
         
-        hdf5_filename = '{}_test.short.hdf5'.format(chr)
-        revcomp_filename = '{}_test.revcomp.short.hdf5'.format(chr)
+        hdf5_filename = '{}_test.hundred.hdf5'.format(chrom)
         print('=> opening {}'.format(hdf5_filename))
         
         with h5py.File(hdf5_filename, 'r+') as hdf5_file:
             group = hdf5_file.create_group('label')
-            dataset = group.create_dataset('data', data=label_array, dtype='uint8')
+            group.create_dataset('data', data=label_array, dtype='uint8')
         print('=> added the labels to {}'.format(hdf5_filename))
-
-        with h5py.File(revcomp_filename, 'r+') as hdf5_file:
-            group = hdf5_file.create_group('label')
-            dataset = group.create_dataset('data', data=label_array, dtype='uint8')
-        print('=> added the labels to {}'.format(revcomp_filename))
 
 
 if __name__ == '__main__':
     print('=> adding labels to the training dataset')
-    collected_labels = collect_labels()
-    add_labels_to_dataset(collected_labels)
+    collected_train_labels = collect_labels()
+    add_labels_to_dataset(collected_train_labels)
 
-    validation_labels = collect_validation_labels()
+    collected_validation_labels = collect_validation_labels()
     print('=> adding labels to the validation dataset')
-    add_labels_to_validation_dataset(validation_labels)
+    add_labels_to_validation_dataset(collected_validation_labels)
 
-    test_labels = collect_test_labels()
+    collected_test_labels = collect_test_labels()
     print('=> adding labels to the testing dataset')
-    add_labels_to_test_dataset(test_labels)
+    add_labels_to_test_dataset(collected_test_labels)
